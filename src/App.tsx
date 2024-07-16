26ee4e66ea09f9b94bdb3c436fcf1dcd3f42c5fe
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-07-17 01:30:11 +0300

    begin task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import './App.css';
 import {withLayout} from "./layout/Layout";
-
+import './styles/globals.css'
 function App() {
   return (
       <h1>Привет мир!</h1>
